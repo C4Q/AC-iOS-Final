32a890871e3a5fb5a2d0ff9f30e5e708c0f16094
Caroline Cruz <cruz.caroline08@gmail.com>
2018-02-26 23:51:26 -0500

    sort postfeed

--- AC-iOS-Final/AC-iOS-Final/Models/ViewControllers/PostFeedViewController.swift
+++ AC-iOS-Final/AC-iOS-Final/Models/ViewControllers/PostFeedViewController.swift
@@ -40,9 +40,9 @@ class PostFeedViewController: UIViewController {
                 }
             }
             // filter out current user's job posts
-            self.posts = posts
+            self.posts = posts.reversed()
 //            self.posts = posts.filter{ $0.userId != AuthUserService.getCurrentUser()?.uid }
-                .sorted{ $0.dateCreated < $1.dateCreated }
+//                .sorted{ $0.dateCreated < $1.dateCreated }
         }
     }
     
